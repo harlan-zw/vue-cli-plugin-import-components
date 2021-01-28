a38dfceb14151143df43ee749ba04ea666ce0448
Harlan Wilton <harlan@harlanzw.com>
2021-01-28 22:24:28 +1100

    chore(example): adding example for setup api (#6)

---
+++ examples/vue3/src/ui/SetupComponent.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="hello">
+  <button @click="inc">
+    <NestedSetupComponent :name="count">Hello</NestedSetupComponent>
+  </button>
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+const inc = () => count.value++
+</script>
