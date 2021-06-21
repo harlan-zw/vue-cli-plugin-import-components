a1dd917bd29df23b414b253f58d3065103c29144
Harlan Wilton <harlan@harlanzw.com>
2021-06-21 21:41:46 +1000

    fix: Vuetify name conflict on installComponents
    Ideally would just use the Vuetify loader installComponents if it's available but having some issues, so we just rename to avoid the conflict.

--- src/vue2/injectComponents.ts
+++ src/vue2/injectComponents.ts
@@ -5,13 +5,13 @@ import type { Component } from '../types'
 const installComponents = require.resolve('./runtime/installComponents')
 
 export default function injectComponents(this: webpackLoader.LoaderContext, source: string, components: Component[]) {
-  const installComponentRuntime = `const installComponents = require(${stringifyRequest(this, `!${installComponents}`)})`
+  const installComponentRuntime = `const vueCliInstallComponent = require(${stringifyRequest(this, `!${installComponents}`)})`
 
   const injectScript
     = `/* vue-cli-plugin-import-components */
 ${installComponentRuntime}
 ${components.map(c => c.import).join('\n')}
-installComponents(component, {${components.map(c => c.pascalName).join(', ')}});`
+vueCliInstallComponent(component, {${components.map(c => c.pascalName).join(', ')}});`
 
   const hotReload = source.indexOf('/* hot reload */')
   if (hotReload > -1)
